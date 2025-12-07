e3a9590176a741ed1469d7c032d3c2c66db45482
Guilherme Pappi <guilhermepappi@MacBook-Air-de-Guilherme.local>
2025-12-06 23:11:24 -0300

    add RIR section + Ajusta receitas

--- src/components/homepage/section-receitas/index.jsx
+++ src/components/homepage/section-receitas/index.jsx
@@ -1,32 +1,8 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
-import { CustomSwiper } from "@/components/common/swiper";
-import { ReceitasCard } from "./components/receitas-card";
+import Link from "next/link";
 
-export const SectionReceitas = async ({ receitas }) => {
-  const hasReceitas = receitas && receitas.length > 0;
-
-  const subtitleContent = hasReceitas ? (
-    <>
-      <p className={styles.paragrafoTitulo}>
-        Doriana está com uma receita, mais saborosa e muito mais cremosa. Pãozinho, torrada, bolo, tudo fica mais gostoso com a Doriana. E toda receita que você prepara com ela, também. #PodeEspalhar.
-      </p>
-    </>
-  ) : (
-    <>No momento não temos receitas cadastradas.</>
-  );
-
-  const slides = hasReceitas
-    ? receitas.map((receita) => (
-        <ReceitasCard
-          key={receita.nome}
-          nome={receita.nome}
-          imagem={receita.imagem?.url || "/receitas/default.png"}
-          href={receita.linkReceita?.url}
-        />
-      ))
-    : null;
-
+export const SectionReceitas = async () => {
   return (
     <section className={styles.receitasSection}>
       <Image
@@ -44,28 +20,20 @@ export const SectionReceitas = async ({ receitas }) => {
             <h2 className={styles.title}>Nossas receitas</h2>
           </span>
 
-          
+          <p className={styles.paragrafoTitulo}>
+            Doriana está com uma receita mais saborosa e muito mais cremosa.
+            Pãozinho, torrada, bolo… tudo fica mais gostoso com Doriana.
+            E toda receita que você prepara com ela, também. #PodeEspalhar
+          </p>
 
-          <section className={styles.subtitle}>{subtitleContent}</section>
-
-          {hasReceitas && (
-            <div className={styles.carouselWrapper}>
-              <CustomSwiper
-                slides={slides}
-                loop
-                navigation
-                pagination={false}
-                autoplay={true}
-                autoplayDelay={3500}
-                spaceBetween={24}
-                breakpoints={{
-                  768: { slidesPerView: 2 },
-                  1024: { slidesPerView: 3 },
-                }}
-                className={styles.receitasSwiper}
-              />
-            </div>
-          )}
+          <Link
+            href="https://www.seara.com.br/marcas/doriana/"
+            target="_blank"
+          >
+            <button className={styles.button}>
+              Conheça nossas receitas
+            </button>
+          </Link>
         </div>
       </div>
     </section>
